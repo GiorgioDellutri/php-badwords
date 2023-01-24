173417e286e6935278ba02a022fdf8fdeefa6d5f
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-24 16:34:36 +0100

    censored text

--- word_censor.php
+++ word_censor.php
@@ -5,3 +5,8 @@ $censored_word = $POST["censored_word"];
 echo "Entered text" . $input_text;
 
 echo "The length of the entered text is" . strlen($input_text) . "characters";
+
+$censored_text = str_replace($censored_word, "***", $input_text);
+
+echo "Censored text" . $censored_text;
+echo "The length of the entered text is" . strlen($censored_text) . "characters";
